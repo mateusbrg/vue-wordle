6119c3732b27780fecb2fcc9ee1347cd7a09f039
Mateus Braga <teteu040506@gmail.com>
2024-11-02 22:03:50 -0300

    refactor(test): extract constants

--- src/components/__tests__/WordlwBoard.spec.ts
+++ src/components/__tests__/WordlwBoard.spec.ts
@@ -2,17 +2,20 @@ import { describe, expect, test } from 'vitest'
 import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
 
+const VICTORY_MESSAGE = 'You won!'
+const TEST_WORD = 'TESTS'
+
 describe('WordleBoard', () => {
   test('a victory message appears when user makes a guess that matches the word of the day', async () => {
     // arrange
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: 'TESTS' } })
+    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: TEST_WORD } })
 
     // act
     const guessInput = wrapper.find('input[type=text]')
-    await guessInput.setValue('TESTS')
+    await guessInput.setValue(TEST_WORD)
     await guessInput.trigger('keydown.enter')
 
     // Assert
-    expect(wrapper.text()).toContain('You won!')
+    expect(wrapper.text()).toContain(VICTORY_MESSAGE)
   })
 })
